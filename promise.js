42b7e1b2358875aaaebd33f43da59b66f9eb16d6
kumaraakarshan <aakarshan0005@gmail.com>
2023-08-01 21:55:31 +0530

    changed using async await

--- promise.js
+++ promise.js
@@ -1,20 +1,23 @@
-const posts=[{title:'POST1'}];
-let lastActivityTime =null;
+const posts = [{ title: 'POST1' }];
+let lastActivityTime = null;
 
-function createPost(post){
-    return new Promise(function (resolve, reject){
-        setTimeout(function(){
+function createPost(post) {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () {
             posts.push(post);
             resolve();
         }, 1000);
     });
 }
+
 function create2ndPost() {
     return createPost({ title: 'POST2' });
 }
+
 function create3rdPost() {
     return createPost({ title: 'POST3' });
 }
+
 function deletePost() {
     return new Promise(function (resolve, reject) {
         setTimeout(function () {
@@ -28,31 +31,34 @@ function deletePost() {
     });
 }
 
-function updateUserActivity(){
-    return new Promise((resolve)=>{
-        setTimeout(()=>{
-            lastActivityTime=new Date();
+function updateUserActivity() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            lastActivityTime = new Date();
             resolve();
-        }, 1000)
-    })
+        }, 1000);
+    });
 }
 
-create2ndPost().then(()=>{
-    return Promise.all([create3rdPost(),updateUserActivity()]);
-})
-.then(()=>{
-    posts.forEach((post)=>{
-        console.log(post.title);
-    });
-    console.log("Last user activity time:",lastActivityTime);
-    return deletePost();
-})
-.then(()=>{
-    console.log("Deleted last post");
+const post_happen =async () => {
+    try {
+        await create2ndPost();
+        await Promise.all([create3rdPost(), updateUserActivity()]);
+
+        posts.forEach((post) => {
+            console.log(post.title);
+        });
+        console.log("Last user activity time:", lastActivityTime);
+
+        await deletePost();
+        console.log("Deleted last post");
         console.log("New set of posts of user are:");
         posts.forEach((post) => {
             console.log(post.title);
         });
+    } catch (err) {
+        console.error(err);
+    }
+};
+post_happen();
 
-})
-.catch((err) => console.error(err));
